8cf7bf071ee6929338f131595f98fa4eb8e04339
UENO, M. <eunosium-1128@live.jp>
2026-07-30 12:57:24 +0900

    Update CMake configuration for Vulkan support
    Added fallback data directories for Vulkan manifests.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,10 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -GNinja .. 
+# Vulkan ICD and layer manifests in conda environments are installed under `$PREFIX/share/vulkan`.
+cmake ${CMAKE_ARGS} -GNinja \
+  -DFALLBACK_DATA_DIRS="${PREFIX}/share:/usr/local/share:/usr/share" \
+  ..
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
